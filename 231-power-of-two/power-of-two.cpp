8c8b484cd4cfe37fda14ef6fefc118850d7777db
Prateek Raj <97014514+prateek2k2@users.noreply.github.com>
2023-10-01 16:55:27 +0530

    Time: 0 ms (100.00%) | Memory: 6.5 MB (9.61%) - LeetSync

---
+++ 231-power-of-two/power-of-two.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if(n==0)
+        return false;
+        if(n==1)
+        return true;
+        else
+        {
+        while(n!=1)
+        {
+            if(n%2!=0)
+            return 0;
+            else
+            n=n/2;
+        }
+        }
+        return n;
+        
+    }
+};
