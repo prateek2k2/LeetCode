b20b3d61e40f3f78611e8e630b71091602fecff8
Prateek Raj <97014514+prateek2k2@users.noreply.github.com>
2023-10-02 22:41:41 +0530

    Time: 34 ms (57.13%) | Memory: 13.7 MB (17.42%) - LeetSync

--- 2149-remove-colored-pieces-if-both-neighbors-are-the-same-color/remove-colored-pieces-if-both-neighbors-are-the-same-color.cpp
+++ 2149-remove-colored-pieces-if-both-neighbors-are-the-same-color/remove-colored-pieces-if-both-neighbors-are-the-same-color.cpp
@@ -1,21 +1,28 @@
 class Solution {
 public:
     bool winnerOfGame(string colors) {
-           int n = colors.length();
-        int aliceWins = 0, bobWins = 0;
-        for (int i = 1; i < n - 1; ++i) {
-            if (colors[i - 1] == 'A' && colors[i] == 'A' && colors[i + 1] == 'A') {
-                aliceWins++;
-            } else if (colors[i - 1] == 'B' && colors[i] == 'B' && colors[i + 1] == 'B') {
-                bobWins++;
+        int alice_plays = 0, bob_plays = 0, count = 0;
+        
+        for (int i = 1; i < colors.size(); i++) {
+            if (colors[i] == colors[i - 1]) {
+                count++;
+            } else {
+                if (colors[i - 1] == 'A') {
+                    alice_plays += std::max(0, count - 1);
+                } else {
+                    bob_plays += std::max(0, count - 1);
+                }
+                count = 0;
             }
         }
-        
-        if (aliceWins > bobWins) {
-            return true;
+
+        if (colors.back() == 'A') {
+            alice_plays += std::max(0, count - 1);
+        } else {
+            bob_plays += std::max(0, count - 1);
         }
         
-        return false;
+        return alice_plays > bob_plays;
         
     }
 };
