bd6098b1e96cb58049930dc4a8db9f1f033b00a0
Prateek Raj <97014514+prateek2k2@users.noreply.github.com>
2023-10-14 23:22:36 +0530

    Time: 2 ms (46.17%) | Memory: 6.4 MB (33.36%) - LeetSync

--- 434-number-of-segments-in-a-string/number-of-segments-in-a-string.cpp
+++ 434-number-of-segments-in-a-string/number-of-segments-in-a-string.cpp
@@ -6,29 +6,29 @@ public:
 
         int ans = 0;
 
-        bool flag = 0;
+        bool temp = 0;
 
         for(int i = 0; i < n; i++) {
 
-            if(s[i] != ' ' && flag == 1 && i != n - 1) 
+            if(s[i] != ' ' && temp == 1 && i != n - 1) 
             continue;
 
-            if(s[i] == ' ' && flag == 0) 
+            if(s[i] == ' ' && temp == 0) 
             continue;
 
-            if(s[i] != ' ' && flag == 0) {
+            if(s[i] != ' ' && temp == 0) {
 
-                flag = 1;
+                temp = 1;
             }
 
-            else if(s[i] == ' ' && flag == 1) {
+            else if(s[i] == ' ' && temp == 1) {
 
-                flag = 0;
+                temp = 0;
 
                 ans++;
             }
 
-            if(i == n - 1 && flag == 1) 
+            if(i == n - 1 && temp == 1) 
             ans++;
         }
 
