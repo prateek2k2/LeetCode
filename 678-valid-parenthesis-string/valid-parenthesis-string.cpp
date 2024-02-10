3b284b5b74d11c4ee4b9e0c4fcfbbfb165e6d1da
Prateek Raj <97014514+prateek2k2@users.noreply.github.com>
2024-02-10 10:35:37 +0530

    Time: 0 ms (100.00%) | Memory: 7.6 MB (14.06%) - LeetSync

--- 678-valid-parenthesis-string/valid-parenthesis-string.cpp
+++ 678-valid-parenthesis-string/valid-parenthesis-string.cpp
@@ -5,18 +5,10 @@ public:
 
         for(int i=0; i<size(s); i++)
         {
-            //push the index of '('
             if(s[i] == '(')
                 open.push(i);
-
-            //push the index of '*'
             else if(s[i] == '*')
                 star.push(i);
-
-            /*If we get a ')' then
-                either pop '(' if we have some
-                or pop '*' if there's no '('.
-            */
             else{
                 if(!open.empty())
                     open.pop();
@@ -24,15 +16,10 @@ public:
                 else if(!star.empty())
                     star.pop();
             
-                //If both '(' ans '*' are exhausted then return false.
                 else
                     return false;
             }
         }
-
-        //Processingthe leftover brackets in 'open'
-
-        /*compare the position of '*' ans '(' if position of '*' is greater than '(' then it means we can convert some '*' to ')'.*/
         while(!open.empty() && !star.empty() && star.top() > open.top())
         {
             open.pop();
